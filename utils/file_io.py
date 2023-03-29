e7b65b915abcd1c795235b63558f6decde7feb4c
Rekorn <urun4m0r1@gmail.com>
2023-03-29 21:45:04 +0900

    🎨 파일 인코딩 기본값 지정

--- utils/file_io.py
+++ utils/file_io.py
@@ -1,29 +1,27 @@
 import json
 
-FILE_ENCODING = "utf-8"
-
 
 def load_json(path: str) -> dict:
     try:
-        with open(path, 'r', encoding=FILE_ENCODING) as f:
+        with open(path, 'r', encoding='utf-8') as f:
             return json.load(f)
     except FileNotFoundError:
         return {}
 
 
 def save_json(path: str, data: dict):
-    with open(path, 'w', encoding=FILE_ENCODING) as f:
+    with open(path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 def load_txt(path: str) -> str:
     try:
-        with open(path, 'r', encoding=FILE_ENCODING) as f:
+        with open(path, 'r', encoding='utf-8') as f:
             return f.read()
     except FileNotFoundError:
         return ''
 
 
 def save_txt(path: str, text: str):
-    with open(path, 'w', encoding=FILE_ENCODING) as f:
+    with open(path, 'w', encoding='utf-8') as f:
         f.write(text)
