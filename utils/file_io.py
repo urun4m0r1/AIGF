5baed68c532bc6ed2c8d59236bb43f8d5cafd4e4
Rekorn <urun4m0r1@gmail.com>
2023-03-30 22:25:38 +0900

    ⚡️ 파일 입출력 개선

--- utils/file_io.py
+++ utils/file_io.py
@@ -1,35 +1,30 @@
 import json
-import os
+from pathlib import Path
+from typing import Dict, Union
+
+PathLike = Union[str, Path]
 
 
-def remove_file(path: str):
+def remove_file(path: PathLike) -> None:
+    Path(path).unlink(missing_ok=True)
+
+
+def load_json(path: PathLike) -> Dict:
     try:
-        os.remove(path)
-    except FileNotFoundError:
-        pass
-
-
-def load_json(path: str) -> dict:
-    try:
-        with open(path, 'r', encoding='utf-8') as f:
+        with Path(path).open(encoding='utf-8') as f:
             return json.load(f)
     except FileNotFoundError:
         return {}
 
 
-def save_json(path: str, data: dict):
-    with open(path, 'w', encoding='utf-8') as f:
+def save_json(path: PathLike, data: Dict) -> None:
+    with Path(path).open('w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-def load_txt(path: str) -> str:
-    try:
-        with open(path, 'r', encoding='utf-8') as f:
-            return f.read()
-    except FileNotFoundError:
-        return ''
+def load_txt(path: PathLike) -> str:
+    return Path(path).read_text(encoding='utf-8', errors='ignore')
 
 
-def save_txt(path: str, text: str):
-    with open(path, 'w', encoding='utf-8') as f:
-        f.write(text)
+def save_txt(path: PathLike, text: str) -> None:
+    Path(path).write_text(text, encoding='utf-8')
