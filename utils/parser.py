42335413bd9826616cba4b02155ca82768924ec9
Rekorn <urun4m0r1@gmail.com>
2023-03-30 23:24:20 +0900

    ⚡️ 파싱 구문 개선

--- utils/parser.py
+++ utils/parser.py
@@ -1,29 +1,30 @@
-from typing import Optional
+from typing import Optional, List, Type, Union
 
 import discord
 
-
-def parse_session_list(value: str) -> list:
-    value = value.strip()
-    if value == '':
-        return []
-
-    return [int(item) for item in value.splitlines()]
+Number = Type[Union[int, float]]
 
 
-def parse_guilds(items: list) -> list:
+def parse_session_list(value: str) -> List[int]:
+    """Parse a string containing session list and return a list of integers."""
+    return [int(item) for item in value.strip().splitlines() if item]
+
+
+def parse_guilds(items: List[str]) -> List[discord.Object]:
+    """Convert a list of strings to a list of discord.Object instances."""
     return [discord.Object(id=int(item)) for item in items]
 
 
+def try_parse_number(text: Optional[str], number_type: Number) -> Optional[Number]:
+    """Attempt to parse a number from a string, returning None if not possible."""
+    return number_type(text) if text is not None and text.isnumeric() else None
+
+
 def try_parse_int(text: Optional[str]) -> Optional[int]:
-    try:
-        return int(text)
-    except (ValueError, TypeError):
-        return None
+    """Attempt to parse an integer from a string, returning None if not possible."""
+    return try_parse_number(text, int)
 
 
 def try_parse_float(text: Optional[str]) -> Optional[float]:
-    try:
-        return float(text)
-    except (ValueError, TypeError):
-        return None
+    """Attempt to parse a float from a string, returning None if not possible."""
+    return try_parse_number(text, float)
