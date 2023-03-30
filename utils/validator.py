4d2a33054596466aacedf81eece68798f0adb527
Rekorn <urun4m0r1@gmail.com>
2023-03-30 23:24:26 +0900

    ⚡️ 유효성 검사 구문 개선

--- utils/validator.py
+++ utils/validator.py
@@ -1,29 +1,30 @@
 from typing import Optional
+from numbers import Real
 
 
 def is_text(value: Optional[str]) -> bool:
-    return isinstance(value, str) and value.strip() != ''
+    return isinstance(value, str) and value.strip()
 
 
-def is_int(value: Optional[int]) -> bool:
+def is_int(value: Optional[Real]) -> bool:
     return isinstance(value, int)
 
 
-def is_float(value: Optional[float]) -> bool:
+def is_float(value: Optional[Real]) -> bool:
     return isinstance(value, float)
 
 
-def is_between_ii(value: Optional[int], min_range: int, max_value: int) -> bool:
-    return is_int(value) and min_range <= value <= max_value
+def is_between_ii(value: Optional[Real], min_value: Real, max_value: Real) -> bool:
+    return isinstance(value, Real) and min_value <= value <= max_value
 
 
-def is_between_ie(value: Optional[int], min_range: int, max_value: int) -> bool:
-    return is_int(value) and min_range <= value < max_value
+def is_between_ie(value: Optional[Real], min_value: Real, max_value: Real) -> bool:
+    return isinstance(value, Real) and min_value <= value < max_value
 
 
-def is_between_ei(value: Optional[int], min_range: int, max_value: int) -> bool:
-    return is_int(value) and min_range < value <= max_value
+def is_between_ei(value: Optional[Real], min_value: Real, max_value: Real) -> bool:
+    return isinstance(value, Real) and min_value < value <= max_value
 
 
-def is_between_ee(value: Optional[int], min_range: int, max_value: int) -> bool:
-    return is_int(value) and min_range < value < max_value
+def is_between_ee(value: Optional[Real], min_value: Real, max_value: Real) -> bool:
+    return isinstance(value, Real) and min_value < value < max_value
