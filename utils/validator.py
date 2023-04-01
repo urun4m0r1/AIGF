b3b4123f702ccc5002c0ca00b0085e058a8bca6a
Rekorn <urun4m0r1@gmail.com>
2023-04-02 02:28:49 +0900

    ✨ IsBetween 메서드 추가

--- utils/validator.py
+++ utils/validator.py
@@ -14,6 +14,19 @@ def is_float(value: Optional[Real]) -> bool:
     return isinstance(value, float)
 
 
+def is_between(value: Optional[Real], min_value: Real, max_value: Real,
+               left_inclusive: bool = True,
+               right_inclusive: bool = False) -> bool:
+    if left_inclusive and right_inclusive:
+        return is_between_ii(value, min_value, max_value)
+    elif left_inclusive and not right_inclusive:
+        return is_between_ie(value, min_value, max_value)
+    elif not left_inclusive and right_inclusive:
+        return is_between_ei(value, min_value, max_value)
+    else:
+        return is_between_ee(value, min_value, max_value)
+
+
 def is_between_ii(value: Optional[Real], min_value: Real, max_value: Real) -> bool:
     return isinstance(value, Real) and min_value <= value <= max_value
 
