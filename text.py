cac01e70f610e802153a1291027c79a24e57feb4
Rekorn <urun4m0r1@gmail.com>
2023-04-04 01:27:05 +0900

    🚧 데이터 클래스 이용 코드 정리

--- text.py
+++ text.py
@@ -1,70 +1,168 @@
-from typing import Dict, List, Optional
+from dataclasses import dataclass
+from typing import List, Optional, Dict, Any
 
 from utils.file_io import load_json
 
 
-class Conversation:
-    def __init__(self, data: Dict):
-        self.participants = data['conversation']['participants']
-        self.names = [p['name'] for p in self.participants]
-        self.personality = data['conversation']['personality']
-        self.prompt = data['conversation']['prompt']
-        self.messages = data['messages']
+@dataclass
+class Participant:
+    role: str
+    name: str
+    nickname: Optional[str] = None
 
 
+@dataclass
 class Personality:
-    def __init__(self, prompt: Dict, names: List[str]):
-        self.personality = prompt['personality']
-        self.categories = {p['category']: p for p in self.personality}
-        self.names = names
+    category: str
+    type: str
 
-    def get_category_sentences(self, category: str, trait_type: str) -> List[str]:
-        return [f'{sentence.format(*self.names)}\n'
-                for traits in self.categories[category]['traits']
-                for sentence in traits['sentences']
-                if traits['type'] == trait_type]
 
-    def get_personality_sentences(self, types: Dict[str, str]) -> str:
-        for category, trait_type in types.items():
-            if trait_type == 'none':
-                continue
+@dataclass
+class Conversation:
+    participants: List[Participant]
+    personality: List[Personality]
+    prompt: str
+    creativity_level: int
+    senders_swapped: bool
 
-            sentences = self.get_category_sentences(category, trait_type)
-            if sentences:
-                yield f"[{category.capitalize()}]\n{''.join(sentences)}\n"
 
-    def get_complete_personality(self, personality: List[Dict]) -> str:
-        return ''.join(self.get_personality_sentences({p['category']: p['type'] for p in personality}))
+@dataclass
+class Message:
+    sender: str
+    text: str
+    timestamp: str
+
+
+@dataclass
+class Messages:
+    messages: List[Message]
+
+
+@dataclass
+class TraitData:
+    type: str
+    sentences: List[str]
+
+
+@dataclass
+class CategoryData:
+    category: str
+    traits: List[TraitData]
+
+
+@dataclass
+class PersonalityData:
+    categories: List[CategoryData]
+
+
+class ConversationParser:
+    def __init__(self, data: Dict[str, Any]):
+        self.data = data
+
+    def parse(self) -> Conversation:
+        participants_data = self.data['conversation']['participants']
+        participants = [Participant(p['role'], p['name'], p.get('nickname')) for p in participants_data]
+        personality_data = self.data['conversation']['personality']
+        personality = [Personality(p['category'], p['type']) for p in personality_data]
+        prompt = self.data['conversation']['prompt']
+        creativity_level = self.data['conversation']['creativityLevel']
+        senders_swapped = self.data['conversation']['sendersSwapped']
+
+        return Conversation(participants, personality, prompt, creativity_level, senders_swapped)
+
+
+class MessagesParser:
+    def __init__(self, data: Dict[str, Any]):
+        self.data = data
+
+    def parse(self) -> Messages:
+        messages_data = self.data['messages']
+        messages = [Message(m['sender'], m['text'], m['timestamp']) for m in messages_data]
+
+        return Messages(messages)
+
+
+class PersonalityParser:
+    def __init__(self, prompt: Dict[str, Any]):
+        self.prompt = prompt
+
+    def parse(self) -> PersonalityData:
+        personality_data = self.prompt['personality']
+
+        categories = []
+        for category_data in personality_data:
+            category_name = category_data['category']
+            traits_data = category_data['traits']
+
+            traits = [TraitData(trait_data['type'], trait_data['sentences']) for trait_data in traits_data]
+            categories.append(CategoryData(category_name, traits))
+
+        return PersonalityData(categories)
 
 
 class OutputGenerator:
-    def __init__(self, conversation: Conversation, personality: Personality):
+    def __init__(self, conversation: Conversation, messages: Messages, personality: PersonalityData):
         self.conversation = conversation
+        self.messages = messages
         self.personality = personality
+        self.names = [p.name for p in self.conversation.participants]
+
+    def get_category_sentences(self, personality: Personality) -> List[str]:
+        return [f"{sentence.format(*self.names)}\n"
+                for category in self.personality.categories
+                if category.category == personality.category
+                for trait in category.traits
+                if trait.type == personality.type
+                for sentence in trait.sentences]
+
+    def get_personality_sentences(self, personality: List[Personality]) -> str:
+        for p in personality:
+            if p.type == 'none':
+                continue
+
+            sentences = self.get_category_sentences(p)
+            if sentences:
+                yield f"[{p.category.capitalize()}]\n{''.join(sentences)}\n"
+
+    def get_complete_personality(self, personality: List[Personality]) -> str:
+        return ''.join(self.get_personality_sentences(personality))
 
     def get_sender_name(self, sender: str) -> Optional[str]:
-        return next((p['name'] for p in self.conversation.participants if p['role'] == sender), None)
+        return next((p.name for p in self.conversation.participants if p.role == sender), None)
+
+    def format_names(self, message: str) -> str:
+        return message.format(*self.names)
 
     def generate_output(self) -> str:
         # Extract personality
-        personality_output = self.personality.get_complete_personality(self.conversation.personality)
+        personality_output = self.get_complete_personality(self.conversation.personality)
 
         # Extract additional
-        additional_output = f"[Additional]\n{self.conversation.prompt.format(*self.conversation.names)}\n\n"
+        additional_output = f"[Additional]\n{self.format_names(self.conversation.prompt)}\n\n"
 
         # Extract messages
         messages_output = '[Messages]\n' + ''.join(
-            [f"{self.get_sender_name(msg['sender'])}: {msg['text']}\n" if self.get_sender_name(msg['sender'])
-             else f"\n({msg['text']})\n\n"
-             for msg in self.conversation.messages])
+            [f"{self.get_sender_name(m.sender)}: {m.text}\n" if self.get_sender_name(m.sender)
+             else f"\n({m.text})\n\n"
+             for m in self.messages.messages])
 
-        return personality_output + additional_output + messages_output
+        return f"{personality_output}{additional_output}{messages_output}"
 
 
 if __name__ == '__main__':
-    conv = Conversation(load_json('cache/1234567890.json'))
-    pers = Personality(load_json('prompt.json'), conv.names)
-    gen = OutputGenerator(conv, pers)
+    dat = load_json('cache/1234567890.json')
+    prom = load_json('prompt.json')
+
+    conv_parser = ConversationParser(dat)
+    conv = conv_parser.parse()
+
+    msg_parser = MessagesParser(dat)
+    msg = msg_parser.parse()
+
+    pers_parser = PersonalityParser(prom)
+    pers = pers_parser.parse()
+
+    gen = OutputGenerator(conv, msg, pers)
     output = gen.generate_output()
 
     print(output)
