b63777a59d62d681cbcfc4fac5194a6471eba57d
Rekorn <urun4m0r1@gmail.com>
2023-04-05 01:54:17 +0900

    🎨 prompt 상대 경로 파싱 변경

--- data/prompt_parser.py
+++ data/prompt_parser.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 from data.prompt import *
 from utils.file_io import load_yaml
 
+CURRENT_DIR = Path(__file__).parent
 DATA_PATH = 'prompt.yaml'
 
 
 def parse() -> Model:
-    data = load_yaml(DATA_PATH)
+    data = load_yaml(CURRENT_DIR / DATA_PATH)
     return Model.parse_obj(data)
