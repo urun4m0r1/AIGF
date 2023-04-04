d5e9db756a1dd810446e6e327bbe29b95b4a302d
Rekorn <urun4m0r1@gmail.com>
2023-04-05 01:22:37 +0900

    ✨ conversation 파서 추가

--- data/codegen.bat
+++ data/codegen.bat
@@ -1,2 +1,3 @@
 @echo off
 poetry run datamodel-codegen --input-file-type yaml --input prompt.yaml --output prompt.py
+poetry run datamodel-codegen --input-file-type yaml --input conversation.yaml --output conversation.py
